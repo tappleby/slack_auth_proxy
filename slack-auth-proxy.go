010a982ba3890e3401a12bba058b5dcd734752e1
Terry Appleby <terry.appleby@gmail.com>
2014-03-30 22:00:54 -0400

    fix errors

--- slack-auth-proxy.go
+++ slack-auth-proxy.go
@@ -1,8 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
 
 func main() {
 	slack := NewSlackApi("xoxp-2174218295-2174484692-2250524839-0e928d")
--- slack.go
+++ slack.go
@@ -11,7 +11,7 @@ type SlackApi struct {
 }
 
 func NewSlackApi(token string) *SlackApi {
-	api := &SlackApi{ token, http.DefaultClient }
+	api := &SlackApi{ Token: token, httpClient: http.DefaultClient }
 	api.Groups = &GroupService{ api: api }
 
 	return api;
