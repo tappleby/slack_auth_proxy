45de3f58a269549a092b3dc92c0dac17fba59d6c
Terry Appleby <terry.appleby@gmail.com>
2014-03-31 23:34:56 -0400

    add unit test for login url

--- slack/oauth_test.go
+++ slack/oauth_test.go
@@ -6,7 +6,8 @@ import (
 	"testing"
 )
 
-func TestNewOAuthClient(t *testing.T) {
+
+func Test_NewOAuthClient(t *testing.T) {
 	client := slack.NewOAuthClient("foo", "bar", "baz")
 
 	assert.Equal(t, client.ClientId, "foo")
@@ -14,3 +15,24 @@ func TestNewOAuthClient(t *testing.T) {
 	assert.Equal(t, client.RedirectUri, "baz")
 }
 
+func Test_OAuthClient_LoginUrl(t *testing.T) {
+	client := slack.NewOAuthClient("foo", "bar", "baz")
+
+	u := client.LoginUrl("1234")
+	uv := u.Query()
+
+	assert.Contains(t, u.String(), "https://slack.com/oauth/authorize")
+	assert.Equal(t, uv.Get("redirect_uri"), "baz")
+	assert.Equal(t, uv.Get("scope"), "identify")
+	assert.Equal(t, uv.Get("client_id"), "foo")
+	assert.Equal(t, uv.Get("state"), "1234")
+
+	assert.Empty(t, uv.Get("team"));
+
+
+	client.TeamId = "foobar"
+	u = client.LoginUrl("1234")
+	uv = u.Query()
+
+	assert.Equal(t, uv.Get("team"), "foobar")
+}
