6e89aca3a959ad29b03b10c44ed44ff71dfe951c
Terry Appleby <terry.appleby@gmail.com>
2014-03-31 20:46:46 -0400

    move slack api stuff into its own package.

--- slack-auth-proxy.go
+++ slack-auth-proxy.go
@@ -1,22 +1,15 @@
 package main
 
 import "log"
-
+import (
+	"github.com/tappleby/slack-auth-proxy/slack"
+)
 
 func main() {
-	slack := NewSlackApi("xoxp-2176048118-2176048120-2250552618-570941")
 
-	userAuth, _ := slack.Auth.Test()
+	slackClient := slack.NewSlackApi("xoxp-2176048118-2176048120-2250552618-570941")
+
+	userAuth, _ :=  slackClient.Auth.Test()
 
 	log.Println(userAuth)
-
-//	groups, _ := slack.Groups.List()
-//	g := groups.FindName("devs")
-
-//	if g != nil {
-//		log.Printf("Found dev group id: %s", g.Id)
-//
-//	} else {
-//		log.Println("Error finding devs group")
-//	}
 }
--- slack/groups.go
+++ slack/groups.go
@@ -1,4 +1,4 @@
-package main
+package slack
 
 type GroupService struct {
 	api *SlackApi
